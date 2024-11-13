aab97a9310de77563aef27512c0a355086726948
Pepes0cool <kaizx8@gmail.com>
2024-11-13 13:05:07 +0700

    Make Identity Authorization

---
+++ SSPS.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SSPS.Models;
+namespace SSPS.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Printer> printers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // need when we use IdentityDbContext
+            modelBuilder.Entity<Printer>().HasData(new Printer { Id = 1, Name = "Printer1", Model = "Model1", SerialNumber = "SerialNumber1", paperNumber = 100 });
+        }
+    }
+}
+
+
---
+++ SSPS.Models/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace SSPS.Models
+{
+    public class Printer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string SerialNumber { get; set; }
+        public int paperNumber { get; set; }
+    }
+}
